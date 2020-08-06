f9373ef5170128609300e84f6606f68f8bf744ca
Aelof3 <folea3@gmail.com>
2020-08-06 16:34:39 -0400

    upload clean

--- assets/js/classes/SinglePlayer.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-import GameState from "./GameState.js";
-
-export default class SinglePlayer extends GameState {
-    constructor( ){
-        super( );
-        this.init();
-    }
-}
